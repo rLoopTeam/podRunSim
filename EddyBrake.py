1031cfa5dd784a125ebe09736b5adfc66a7a8d72
adam <adam.lange@langeturbo.com>
2016-01-13 22:11:28 -0600

    Added interpolation of and H from eddyBrakeData.csv

--- EddyBrake.py
+++ EddyBrake.py
@@ -12,3 +12,17 @@ class EddyBrake():
 
     df_f_lift = df[df['F_lift'].isnull()==False]
     self.f_lift = LinearNDInterpolator((df_f_lift['v'],df_f_lift['h']),df_f_lift['F_lift'])
+
+
+    df_f_H_y_max = df[df['H_y_max'].isnull()==False]
+    self.H_y_max = LinearNDInterpolator((df_f_H_y_max['v'],df_f_H_y_max['h']),df_f_H_y_max['H_y_max'])
+
+    df_f_H_y_mean = df[df['H_y_mean'].isnull()==False]
+    self.H_y_mean = LinearNDInterpolator((df_f_H_y_mean['v'],df_f_H_y_mean['h']),df_f_H_y_mean['H_y_mean'])
+
+    df_f_q_max = df[df['q_max_rail_surf'].isnull()==False]
+    self.q_max = LinearNDInterpolator((df_f_q_max['v'],df_f_q_max['h']),df_f_q_max['q_max_rail_surf'])
+
+    df_f_q_mean = df[df['q_rail_surf_mean'].isnull()==False]
+    self.q_mean = LinearNDInterpolator((df_f_q_mean['v'],df_f_q_mean['h']),df_f_q_mean['q_rail_surf_mean'])
+
