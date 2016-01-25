bae1659466f9127dfbe23a2abddc6183c89d7e87
adam <adam.lange@langeturbo.com>
2016-01-25 13:02:39 -0600

    added temp plot

--- dataConvUtil/plotEddyBrakeData.py
+++ dataConvUtil/plotEddyBrakeData.py
@@ -13,7 +13,7 @@ eb = EddyBrake.EddyBrake(args.infile)
 
 plt.figure()
 
-plt.subplot(211)
+plt.subplot(311)
 for i in df['h'].unique():
   dfi = df[df['h']==i]
   dfi = dfi.sort(['v'])
@@ -26,15 +26,31 @@ for i in df['h'].unique():
 plt.xlabel('velocity [m/s]')
 plt.ylabel('force [N]')
 plt.grid()
-plt.text(15,-750,'contour labels:\nmagnet to rail gap [m]\n- braking force\n-- normal force',fontsize=8)
+plt.text(15,-550,'contour labels:\nmagnet to rail gap [m]\n- braking force\n-- normal force',fontsize=8)
 
-plt.subplot(212)
+plt.subplot(312)
 for i in df['h'].unique():
   dfi = df[df['h']==i]
   dfi = dfi.sort(['v'])
   plt.plot(dfi['v'],dfi['q_max_rail_surf'],'-',label='gap = {}'.format(i))
+  if not np.isnan(eb.q_max(100,i).max()):
+    plt.text(100,eb.q_max(100,i).max(),str(i),fontsize=8,ha='center')
 plt.ylabel('max rail surface heating [W/m^3]')
 plt.xlabel('velocity [m/s]')
 plt.grid()
 
+plt.subplot(313)
+for i in df['h'].unique():
+  dfi = df[df['h']==i]
+  dfi = dfi.sort(['v'])
+  plt.plot(dfi['v'],dfi['H_y_mean'],'-',label='gap = {}'.format(i))
+  if not np.isnan(eb.H_y_mean(50,i).max()):
+    plt.text(50,eb.H_y_mean(50,i).max(),str(i),fontsize=8,ha='center')
+  plt.plot(dfi['v'],dfi['H_y_max'],'-',label='gap = {}'.format(i))
+  if not np.isnan(eb.H_y_max(75,i).max()):
+    plt.text(75.0,eb.H_y_max(75,i).max(),str(i),fontsize=8,ha='center')
+plt.ylabel('H [A/m]')
+plt.xlabel('velocity [m/s]')
+
+plt.grid()
 plt.show()
